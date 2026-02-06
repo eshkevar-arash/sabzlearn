0a98a04c76cabc16635ce2edbdc01e95ed1fcd85
Arash <eshkevar.arash1387@gmail.com>
2026-02-06 21:39:31 +0330

    feat: complete mobile menu HTML and CSS and JS

--- js/app.js
+++ js/app.js
@@ -9,3 +9,21 @@ overlay.addEventListener('click', () => {
     mobileBar.classList.remove('mobile-bar--show')
     overlay.classList.remove('overlay--show')
 })
+
+/*Start Mobile Category Menu*/
+const mobileCategoryMenu = document.querySelector('.mobile-category__menu')
+const mobileCategoryMenuItems = mobileCategoryMenu.querySelectorAll('.mobile-category__menu-item')
+
+mobileCategoryMenu.addEventListener('click', event => {
+    const targetSvg = event.target.closest('.mobile-category__menu-icon')
+    if (targetSvg){
+        const targetLi = targetSvg.parentElement.parentElement
+        mobileCategoryMenuItems.forEach(item => {
+            if (item !== targetLi){
+                item.classList.remove('mobile-category__menu-item--open')
+            }
+        })
+        targetLi.classList.toggle('mobile-category__menu-item--open')
+    }
+})
+/*Finish Mobile Category Menu*/
